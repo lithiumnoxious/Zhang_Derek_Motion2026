0831525bc096699db6abcff726fbd305be6163ed
derek <zhang278@sheridancollege.ca>
2026-09-09 18:34:06 -0400

    task 3 is completed and jornal entry is noted down, I sure hope i make sense. nothing much to note outside of each task being completed, I'm not sure why I couldn't use the exact same method as before regarding the square root / magnitude, I will tamper with this more after this.

--- Assets/scripts/week1/Jornal 1/Pipeline.cs
+++ Assets/scripts/week1/Jornal 1/Pipeline.cs
@@ -1,7 +1,16 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Pipeline : MonoBehaviour
 {
+    public Vector2 intitalpress;
+    public Vector2 mousepos;
+    public float t;
+    public List<Vector2> Listp;
+    public float totallength;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +20,37 @@ public class Pipeline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mousepos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            intitalpress = mousepos;
+            Listp.Add(intitalpress);
+             t = 0;
+        }
+        if (Mouse.current.leftButton.isPressed)
+        {
+            t+= 0.1f * Time.deltaTime;
+            if(t >= 0.1)
+            {
+                Debug.DrawLine(intitalpress,mousepos, Color.white, 10);
+                intitalpress = mousepos;
+                Listp.Add(intitalpress);
+                t = 0;
+            }
+        }
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            for (int i = 0; i < Listp.Count - 1; i++)
+            {
+                Vector2 current = Listp[i];
+                Vector2 next = Listp[i + 1];
+                float CNX = current.x* current.x + next.x*next.x;
+                float CNY = current.y * current.y + next.y * next.y;
+                totallength += Mathf.Sqrt(CNX + CNY);
+            }
+            Debug.Log(totallength);
+            totallength = 0;
+            Listp.Clear();
+        }
     }
 }
