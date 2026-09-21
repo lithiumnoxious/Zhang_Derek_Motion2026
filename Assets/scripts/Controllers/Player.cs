c05a615f3348dc3fe16b5c96261649af7937e224
zhang278 <lithiumnoxious@gmail.com>
2026-09-21 16:00:21 -0400

    continous player movement

--- Assets/scripts/Controllers/Player.cs
+++ Assets/scripts/Controllers/Player.cs
@@ -15,9 +15,22 @@ public class Player : MonoBehaviour
     public int numberOfTrailBombs;
     public float angle;
 
+    public Vector3 currentvelo = Vector3.right;
+    public float speed;
+
+    private void Start()
+    {
+        transform.position = transform.position + currentvelo;
+        //transform.position += Vector3.right;
+
+    }
 
     void Update()
     {
+        //transform.position = transform.position + currentvelo;
+      
+        
+
         if (Keyboard.current.bKey.wasReleasedThisFrame)
         {
             //when b key is pressed the bomb will be instantiated
@@ -47,6 +60,33 @@ public class Player : MonoBehaviour
             DetectAteroids(5, asteroidTransforms); //task 4
         }
 
+        //if (Keyboard.current.leftArrowKey.isPressed)
+        //{
+        //    //PlayerMovement3(-1,0,0);
+        //    PlayerMovement2(Vector3.left);
+        //}
+        //if (Keyboard.current.rightArrowKey.isPressed)
+        //{
+        //    //PlayerMovement3(1, 0, 0);
+        //    PlayerMovement2(Vector3.right);
+
+        //}
+        //if (Keyboard.current.upArrowKey.isPressed)
+        //{
+        //    //PlayerMovement3(0, 1, 0);
+        //    PlayerMovement2(Vector3.up);
+
+        //}
+        //if (Keyboard.current.downArrowKey.isPressed)
+        //{
+        //    //PlayerMovement(0, -1, 0);
+        //    PlayerMovement2(Vector3.down);
+
+        //}
+        if (Keyboard.current.leftArrowKey.isPressed|| Keyboard.current.rightArrowKey.isPressed|| Keyboard.current.upArrowKey.isPressed|| Keyboard.current.downArrowKey.isPressed)
+        {
+            playermovement();
+        }
 
     }
     public void spawnbomboffset(Vector3 inoffset)
@@ -79,20 +119,15 @@ public class Player : MonoBehaviour
         {
             case 0:
                 GameObject tl = Instantiate(bombPrefab, transform.position + Vector3.up + Vector3.left + new Vector3(-inDistance, inDistance, 0).normalized, Quaternion.identity);
-
                 break;
             case 1:
                 GameObject tr = Instantiate(bombPrefab, transform.position + Vector3.up + Vector3.right + new Vector3(inDistance, inDistance, 0).normalized, Quaternion.identity);
-
                 break;
-
             case 2:
                 GameObject bl = Instantiate(bombPrefab, transform.position + Vector3.down + Vector3.left + new Vector3(-inDistance, -inDistance, 0).normalized, Quaternion.identity);
-
                 break;
             case 3:
                 GameObject br = Instantiate(bombPrefab, transform.position + Vector3.down + Vector3.right + new Vector3(inDistance, -inDistance, 0).normalized, Quaternion.identity);
-
                 break;
         }
     }
@@ -102,7 +137,6 @@ public class Player : MonoBehaviour
         Destroy(bob, 3);
         //says bomba so I know it works
         Debug.Log("bomba");
-
         yield return (null);
     }
     public void warpDrive(Vector3 ship, float speed)
@@ -122,7 +156,6 @@ public class Player : MonoBehaviour
         {
             ratio = 0;
         }
-
         transform.position = Vector3.Lerp(transform.position, target.position, ratio);
     }
 
@@ -137,6 +170,37 @@ public class Player : MonoBehaviour
             }
         }
     }
-
-
+    public void PlayerMovement3(int x,int y, int z)
+    {
+        transform.position += new Vector3(x,y,z);
+    }
+    public void PlayerMovement2(Vector3 v)
+    {
+        transform.position += v.normalized * speed * Time.deltaTime;
+    }
+    public void playermovement()
+    {
+        currentvelo = Vector3.zero;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            currentvelo += Vector3.left;
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            currentvelo += Vector3.right;
+        }
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            currentvelo += Vector3.up;
+        }
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+            currentvelo += Vector3.down;
+        }
+        currentvelo = currentvelo.normalized;
+        transform.position += currentvelo.normalized * speed * Time.deltaTime;
+        //normalized is used to keep the continous movement stable
+        //time.deltatime is used instead of frame rate also for stability sake
+        ////furthermore time.deltatime is used when real life seconds matter ie gravity and projectiles
+    }
 }
